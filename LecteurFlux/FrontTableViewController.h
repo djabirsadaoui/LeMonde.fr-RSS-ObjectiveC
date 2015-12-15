8d2c81abb7bf225d4cccf297a7ee5e2f901f1d0d
djabir <djabir@magency.fr>
2015-12-15 18:15:05 +0100

    add new files

---
+++ LecteurFlux/FrontTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrontTableViewController.h
+//  LecteurFlux
+//
+//  Created by djabir sadaoui on 15/12/2015.
+//  Copyright © 2015 djabir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrontTableViewController : UITableViewController
+
+@end
